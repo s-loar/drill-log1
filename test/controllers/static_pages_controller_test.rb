4c9f4ff99a54520a5c37ffde9f36be262c1a4e1e
Steve Loar <sjl7909@gmail.com>
2015-03-24 14:21:42 -0400

    title tests pass

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Home | Login Sample Application"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Login Sample Application"
   end
 
 end
