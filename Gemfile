f1e1c3034b54a13cb3a1808d2a7f5f396f86c9e6
Steve Loar <sjl7909@gmail.com>
2015-03-24 13:52:45 -0400

    working on initial pages

--- Gemfile
+++ Gemfile
@@ -38,7 +38,7 @@ gem 'bootstrap-sass', 					'~> 3.3.4.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  #gem 'byebug'
+  gem 'byebug', '~> 4.0.3'
 
   gem 'pry'
   gem 'pry-nav'
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#index'
+
+  get 'help'    => 'static_pages#help'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
